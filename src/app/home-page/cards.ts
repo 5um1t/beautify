a7a4f802d252e1258e1f2aa27eacaa9a98673804
5um1t <asumitkadam@gmail.com>
2021-05-02 11:31:35 +0530

    assets/img updated to webp

--- src/app/home-page/cards.ts
+++ src/app/home-page/cards.ts
@@ -5,40 +5,40 @@ export const cards: card[] = [
     description:
       'DIOR Lip Glow Hydrating Lip Balm Color-Awakening - 007 Raspberry',
     buttonText: 'Buy Now',
-    img: '../../assets/img/diana-polekhina-SD4f7jBzWfE-unsplash.jpg',
+    img: '../../assets/img/card-1.webp',
   },
   {
     title: 'Neauthy Face Serums',
     description:
       'Neauthy Face Serum 100% Hyaluronic Acid and 96% Pure Snail Slime Anti-Wrinkle for Men and Women Regenerating Moisturising Against Acne Flecks Made in Italy 30 ml',
     buttonText: 'Buy Now',
-    img: 'assets/img/neauthy-skincare-8ZKwW-2SR28-unsplash.jpg',
+    img: 'assets/img/card-2.webp',
   },
   {
     title: 'Brushes',
     description:
       'The soft and fluffy bristles feel smooth and gentle on your skin.',
     buttonText: 'Buy Now',
-    img: '../../assets/img/raphael-lovaski-pxax5WuM7eY-unsplash.jpg',
+    img: '../../assets/img/card-3.webp',
   },
   {
     title: 'Skin Glows',
     description:
       'Super Light Gel Face Moisturiser with Hyaluronic Acid and Vitamin E, 147 g',
     buttonText: 'Buy Now',
-    img: '../../assets/img/crystalweed-cannabis-87v3-xW2MnQ-unsplash.jpg',
+    img: '../../assets/img/card-4.webp',
   },
   {
     title: 'Lips Sticks',
     description:
       'will give your lips a gorgeous rosy glow while providing a surge of moisture with sweet almond and rose oil.',
     buttonText: 'Buy Now',
-    img: 'assets/img/juliana-malta-g_7tdc8SilM-unsplash.jpg',
+    img: 'assets/img/card-5.webp',
   },
   {
     title: 'Mills Makeup Kits',
     description: 'MILK MAKEUP MVPs Set MEET THE ALL STARS LIMITED EDITION',
     buttonText: 'Buy Now',
-    img: 'assets/img/ines-mills-kXj-B6JUlU4-unsplash.jpg',
+    img: 'assets/img/card-6.webp',
   },
 ];
