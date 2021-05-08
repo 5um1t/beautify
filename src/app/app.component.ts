5d54946de810e85836c5d65d03a94ab82d9349e2
Sumit kadam <asumitkadam@gmail.com>
2021-05-08 20:18:55 +0530

    removed unwanted typesccript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
 import { cards } from './home-page/cards';
-import { ParallaxScrollComponent } from './parallax-scroll/parallax-scroll.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
