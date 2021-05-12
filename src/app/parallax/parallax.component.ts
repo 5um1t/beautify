1e6d0a96bf80ae2f7537bc7c142ea73ad670b720
Sumit kadam <asumitkadam@gmail.com>
2021-05-12 16:37:50 +0530

    changed parallax

--- src/app/parallax/parallax.component.ts
+++ src/app/parallax/parallax.component.ts
@@ -9,7 +9,9 @@ export class ParallaxComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    var image = document.getElementsByClassName('thumbnail');
-    new simpleParallax(image);
+    var image = document.getElementsByClassName('thumbnail-right');
+    new simpleParallax(image, { orientation: 'right', scale: 1.6 });
+    var image1 = document.getElementsByClassName('thumbnail-left');
+    new simpleParallax(image1, { orientation: 'left', scale: 1.6 });
   }
 }
